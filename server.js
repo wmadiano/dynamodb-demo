2e3f18f3da9ba043b6cbce430fdf4cb07780dc9b
wmadiano <wmadiano@gmail.com>
2024-04-22 13:14:19 +0800

    changed port to 3000

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { createInvoice } = require('./models/receiptInvoiceModel'); // Ensure the path matches your project structure
-
+const { createInvoice, getAllInvoices } = require('./models/receiptInvoiceModel'); // Ensure the path matches your project structure
+const cors = require('cors');
 const app = express();
-const PORT = 6000;
+
+
+const PORT = 3000;
 
 app.use(bodyParser.json());
+
+app.use(cors());
+
+
+// app.use(cors({
+//   origin: 'http://localhost:6000' // adjust this depending on where your frontend is hosted
+// }));
 
 // POST endpoint to create a new invoice
 app.post('/api/receiptinvoice', async (req, res) => {
@@ -26,6 +35,25 @@ app.post('/api/receiptinvoice', async (req, res) => {
     res.status(500).json({ message: 'Failed to create invoice', error: error.message });
   }
 });
+
+app.get('/', async (req, res) => {
+  try {
+    console.log("Fetching invoices...");
+    const invoices = await getAllInvoices();
+    console.log("Invoices fetched:", invoices);
+    res.status(200).json({
+      message: 'Invoices retrieved successfully',
+      data: invoices
+    });
+  } catch (error) {
+    console.error('Error in retrieving invoices:', error);
+    res.status(500).json({ message: 'Failed to retrieve invoices', error: error.message });
+  }
+});
+
+
+
+
 
 // Start the server
 app.listen(PORT, () => {
